4c7e6fd7271c11518b796e236dd141cfb2615667
jiandong.huang <1131909224m@sina.cn>
2018-12-27 16:32:33 +0800

    删除不必要的配置

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,29 +1,16 @@
 #!/usr/bin/env bash
-ELASTIC_HOST=192.168.1.78
-ELASTIC_PORT=10015
-
-ELASTIC_HOST1=10.10.100.38
-ELASTIC_PORT1=10015
-
-HOSTNAME=`hostname`
+RETVAL=0
+current_path=`pwd`
+file_path=$(dirname $0)
 
 YUM_HOST=10.10.100.38
 YUM_PORT=8080
-GEM_PATH=/tmp/gem-cache
+GEM_PATH=$file_path/gem-cache
 FLUENTD_PORT=24444
 
-GRID_BASE=/opt/ogrid
-GRID_HOME=/opt/grid/products/11.2.0
-ORACLE_BASE=/opt/oracle
-ORACLE_HOME=/opt/oracle/products/11.2.0
-ORACLE_SID=+ASM1
-
-DATABASE_NAME=orcl
-INSTANCE_NAME=orcl1
-
-
 TD_AGENT_PATH=/etc/td-agent
-TD_AGENT_CONF=$TD_AGENT_PATH/conf.d
+TD_AGENT_CONF_PATH=$TD_AGENT_PATH/td-agent.conf
+TD_AGENT_CONFD=$TD_AGENT_PATH/conf.d
 TD_AGENT=/etc/init.d/td-agent
 
 TD_AGENT_LOG_PATH=/var/log/td-agent
@@ -44,9 +31,8 @@ EOF
   number=$(rpm -qa | grep td-agent | wc -l)
   if [ ! $number -gt 0 ];then
     yum install -y td-agent
-    mkdir -p $TD_AGENT_CONF
 
-    cat >>$TD_AGENT_PATH/td-agent.conf<<EOF
+    cat >>$TD_AGENT_CONF_PATH<<EOF
 
 @include conf.d/*.conf
 
@@ -55,10 +41,11 @@ EOF
 </system>
 
 EOF
-
-    sed -i "s/TD_AGENT_USER=td-agent/TD_AGENT_USER=root/g" $TD_AGENT
-    sed -i "s/TD_AGENT_GROUP=td-agent/TD_AGENT_GROUP=root/g" $TD_AGENT
   fi
+  
+  mkdir -p $TD_AGENT_CONFD
+  sed -i "s/TD_AGENT_USER=td-agent/TD_AGENT_USER=root/g" $TD_AGENT
+  sed -i "s/TD_AGENT_GROUP=td-agent/TD_AGENT_GROUP=root/g" $TD_AGENT
 }
 
 install_libcurl() {
@@ -91,6 +78,30 @@ install_elastic_plugin() {
 	fi
 }
 
+install_prometheus_plugin() {
+  print install_prometheus_plugin
+  if [ ! -f $GEM_PATH/fluent-plugin-prometheus*.gem ];then
+    echo -e "\033[31mno such directory $GEM_PATH/fluent-plugin-prometheus*.gem\033[0m"
+  else
+    number=$(td-agent-gem list | grep fluent-plugin-prometheus | wc -l)
+    if [ ! $number -gt 0 ];then
+        cd $GEM_PATH && td-agent-gem install fluent-plugin-prometheus*.gem --local
+    fi
+  fi
+}
+
+
+install_http_plugin() {
+  print fluent-plugin-out-http
+  if [ ! -f $GEM_PATH/fluent-plugin-out-http*.gem ];then
+    echo -e "\033[31mno such directory $GEM_PATH/fluent-plugin-out-http*.gem\033[0m"
+  else
+    number=$(td-agent-gem list | grep fluent-plugin-out-http | wc -l)
+    if [ ! $number -gt 0 ];then
+        cd $GEM_PATH && td-agent-gem install fluent-plugin-out-http*.gem --local
+    fi
+  fi
+}
 
 install_date_plugin() {
   print install_date_plugin
@@ -107,178 +118,6 @@ install_date_plugin() {
   fi
 }
 
-
-configuration_crs_log() {
-  print configuration_crs_log
-
-  ip=$(get_loacl_ip)
-
-  cat >$TD_AGENT_CONF/crs.conf <<EOF
-<source>
-  @type tail
-  path $GRID_HOME/log/$HOSTNAME/alert$HOSTNAME.log
-  pos_file $TD_AGENT_LOG_PATH/crs/crs.log.pos
-  tag crs.log
-
-  format multiline
-  multiline_flush_interval 5s
-  format_firstline /\d{4}-\d{1,2}-\d{1,2}/
-  format1 /^(?<log_time>\d{4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2})\.(?<line>\d+):(?<message>.*)/
-  time_format %Y-%m-%d %H:%M:%S
-</source>
-
-<filter crs.log>
-  @type record_transformer
-  <record>
-    hostname \${hostname}
-    ip $ip
-    log_type crs
-  </record>
-</filter>
-
-<filter crs.log>
-  @type dio
-  keys log_time
-</filter>
-
-<match crs.log>
-  @type copy
-  @include crs.conf.d/*.conf
-</match>
-EOF
-
-  mkdir -p $TD_AGENT_CONF/crs.conf.d
-  cat >$TD_AGENT_CONF/crs.conf.d/$ELASTIC_HOST.conf <<EOF
-<store>
-  @type elasticsearch
-  host $ELASTIC_HOST
-  port $ELASTIC_PORT
-
-  time_key time
-  flush_interval 2s
-  buffer_queue_limit 4096
-  buffer_chunk_limit 1024m
-  num_threads 4
-  logstash_format true
-</store>
-EOF
-
-  cat >$TD_AGENT_CONF/crs.conf.d/$ELASTIC_HOST1.conf <<EOF
-<store>
-  @type elasticsearch
-  host $ELASTIC_HOST1
-  port $ELASTIC_PORT1
-
-  time_key time
-  flush_interval 2s
-  buffer_queue_limit 4096
-  buffer_chunk_limit 1024m
-  num_threads 4
-  logstash_format true
-</store>
-EOF
-}
-
-configuration_asm_log() {
-  print configuration_asm_log
-
-	ip=$(get_loacl_ip)
-
-  cat >$TD_AGENT_CONF/asm.conf <<EOF
-<source>
-  @type tail
-  path $GRID_BASE/diag/asm/+asm/$ORACLE_SID/trace/alert_$ORACLE_SID.log
-  pos_file $TD_AGENT_LOG_PATH/asm/asm.log.pos
-  tag asm.log
-
-  format multiline
-  multiline_flush_interval 5s
-  format_firstline /\w{3} \w{3} \d{2} \d{2}:\d{2}:\d{2} \d{4}/
-  format1 /^\w{3} (?<log_time>\w{3} \d{2} \d{2}:\d{2}:\d{2} \d{4})(?<message>.*)/
-  time_format %a %B %d %H:%M:%S %Y
-</source>
-
-<filter asm.log>
-  @type record_transformer
-  <record>
-    hostname \${hostname}
-    ip $ip
-    log_type asm
-  </record>
-</filter>
-
-<filter asm.log>
-  @type dio
-  keys log_time
-</filter>
-
-<match asm.log>
-  @type elasticsearch
-  host $ELASTIC_HOST
-  port $ELASTIC_PORT
-
-
-  time_format %a %B %d %H:%M:%S %Y
-  time_key time
-  flush_interval 2s
-  buffer_queue_limit 4096
-  buffer_chunk_limit 1024m
-  num_threads 4
-  logstash_format true
-</match>
-EOF
-}
-
-
-configuration_instance_log() {
-  print configuration_instance_log
-  ip=$(get_loacl_ip)
-
-  cat >$TD_AGENT_CONF/instance.conf <<EOF
-<source>
-  @type tail
-  path $ORACLE_BASE/diag/rdbms/$DATABASE_NAME/$INSTANCE_NAME/trace/alert_$INSTANCE_NAME.log
-  pos_file $TD_AGENT_LOG_PATH/instance/instance.log.pos
-  tag instance.log
-
-  format multiline
-  multiline_flush_interval 5s
-  format_firstline /\w{3} \w{3} \d{2} \d{2}:\d{2}:\d{2} \d{4}/
-  format1 /^\w{3} (?<log_time>\w{3} \d{2} \d{2}:\d{2}:\d{2} \d{4})(?<message>.*)/
-  time_format %a %B %d %H:%M:%S %Y
-</source>
-
-<filter instance.log>
-  @type record_transformer
-  <record>
-    hostname \${hostname}
-    ip $ip
-    log_type instance
-  </record>
-</filter>
-
-<filter instance.log>
-  @type dio
-  keys log_time
-</filter>
-
-<match instance.log>
-  @type elasticsearch
-  host $ELASTIC_HOST
-  port $ELASTIC_PORT
-
-  
-  time_format %a %B %d %H:%M:%S %Y
-  time_key time
-  flush_interval 2s
-  buffer_queue_limit 4096
-  buffer_chunk_limit 1024m
-  num_threads 4
-  logstash_format true
-</match>
-EOF
-}
-
 get_loacl_ip() {
 	ip=`ip a | grep inet | grep -v "127.0.0.1" | grep -v "::1" | grep -v "ib" | grep -v "docker" | grep -v "inet6 "| grep -v ":" | awk '{print$2}' | awk -F "/" '{print $1}'`
 	echo $ip
@@ -290,7 +129,7 @@ print () {
 
 # 打印帮助信息
 print_help() {
-    echo "Usage: bash $0 { all | install_td_agent | install_elastic_plugin | install_date_plugin | configuration_crs_log | configuration_asm_log | configuration_instance_log }"
+    echo "Usage: bash $0 { all | install_td_agent | install_plugin }"
     echo -e "\033[32me.g: bash $0 install_td_agent\033[0m"
 }
 
@@ -315,30 +154,32 @@ main() {
         case "${func_name}" in
           all)
             install_td_agent
+
             install_elastic_plugin
             install_date_plugin
-
-            configuration_crs_log
-            configuration_asm_log
-            configuration_instance_log
+            install_prometheus_plugin
+            install_http_plugin
+            ;;
+          install_plugin)
+            install_elastic_plugin
+            install_date_plugin
+            install_prometheus_plugin
+            install_http_plugin
             ;;
           install_td_agent)
             install_td_agent
             ;;
-          install_date_plugin)
-            install_date_plugin
-            ;;
           install_elastic_plugin)
             install_elastic_plugin
             ;;
-          configuration_crs_log)
-            configuration_crs_log
+          install_date_plugin)
+            install_date_plugin
             ;;
-          configuration_asm_log)
-            configuration_asm_log
+          install_prometheus_plugin)
+            install_prometheus_plugin
             ;;
-          configuration_instance_log)
-            configuration_instance_log
+          install_http_plugin)
+            install_http_plugin
             ;;
           *)  # 匹配都失败执行
             print_help
--- client/pre-td-agent.sh
+++ client/pre-td-agent.sh
@@ -1,18 +1,41 @@
 #!/usr/bin/env bash
+RETVAL=0
+current_path=`pwd`
+file_path=$(dirname $0)
 
-yum install -y sudo
+print () {
+    echo -e "\033[32m$1\033[0m"
+}
 
-curl -L https://toolbelt.treasuredata.com/sh/install-redhat-td-agent2.sh | sh
+install_env() {
+    print "install env"
+    yum install -y sudo
+    yum install -y libcurl-devel gcc
+}
 
-yum install -y libcurl-devel gcc
+install_target() {
+    print "install target"
+    # curl -L https://toolbelt.treasuredata.com/sh/install-redhat-td-agent2.sh | sh
+    bash $file_path/deploy.sh install_td_agent
+}
 
-td-agent-gem install fluent-plugin-dio
+install_plugin () {
+    print "install plugin"
+    td-agent-gem install fluent-plugin-elasticsearch
+    td-agent-gem install fluent-plugin-dio
+    td-agent-gem install fluent-plugin-out-http
+    td-agent-gem install fluent-plugin-prometheus
+}
 
-td-agent-gem install fluent-plugin-elasticsearch
+copy_gem_cache() {
+    print "copy gem chache"
+    rm -rf /tmp/gem-cache
+    cp -r /opt/td-agent/embedded/lib/ruby/gems/2.1.0/cache /tmp/gem-cache
+}
 
-td-agent-gem install fluent-plugin-out-http
-
-rm -rf /tmp/gem-cache
-
-cp -r /opt/td-agent/embedded/lib/ruby/gems/2.1.0/cache /tmp/gem-cache
-
+install_env
+install_target
+install_plugin
+copy_gem_cache
+print "done!"
+exit $RETVAL
